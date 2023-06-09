d699e0cf94b4578dd46962b166775b3adccf4fbd
MrGoopyDrawers <107856947+MrGoopyDrawers@users.noreply.github.com>
2023-06-08 21:39:25 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BananzaBTD6;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.1";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "Bananza";
 
     public const string Description = "Adds the bananza gamemode to BTD6. Gives you DOUBLE cash generation, more starting cash to jumpstart your farming early game, and sets the end round to round 200 instead of 100. Pair this with other mods to recreate the BTDB fun!";
